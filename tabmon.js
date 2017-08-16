73ff2e1a93b2cf2ebb92c609f8edd824692639d9
Mike Wu <dev@mgw.io>
2017-08-16 02:33:50 -0400

    Fix default newtab; optimize process to make page active

--- tabmon.js
+++ tabmon.js
@@ -1,16 +1,21 @@
 
 function nt (newtab) {
 	function waitForURL(tabId, changeInfo, tab) {
-		console.log(changeInfo);
 		if (changeInfo.title === '@NewTab') {
 			browser.storage.local.get('newtaburl').then(function (ntu) {
 				browser.tabs.onUpdated.removeListener(waitForURL);
+				if (ntu.newtaburl == null) {
+					browser.storage.local.set({
+						newtaburl: 'about:blank'
+					});
+					ntu.newtaburl = 'about:blank';
+				}
 				browser.storage.local.get('active').then(function (act) {
 					if (act.active) {
-						browser.tabs.remove(newtab.id);
 						browser.tabs.create({
 							'url': ntu.newtaburl
-						})
+						});
+						browser.tabs.remove(newtab.id);
 					} else {
 						browser.tabs.update(tabId, {
 							'url': ntu.newtaburl
