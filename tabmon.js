0d9a91d898fdf79d5509abd82b10dcfd7f148b0f
Mike Wu <dev@mgw.io>
2017-08-20 17:36:35 -0400

    Remove placeholder from tab history after close

--- tabmon.js
+++ tabmon.js
@@ -2,7 +2,7 @@
 function nt (newtab) {
 	function waitForURL(tabId, changeInfo, tab) {
 		if (changeInfo.title === '@NewTab') {
-			browser.storage.local.get('newtaburl').then(function (ntu) {
+			browser.storage.local.get('newtaburl').then((ntu) => {
 				browser.tabs.onUpdated.removeListener(waitForURL);
 				if (ntu.newtaburl == null) {
 					browser.storage.local.set({
@@ -10,12 +10,22 @@ function nt (newtab) {
 					});
 					ntu.newtaburl = 'about:blank';
 				}
-				browser.storage.local.get('active').then(function (act) {
+				browser.storage.local.get('active').then((act) => {
 					if (act.active) {
 						browser.tabs.create({
 							'url': ntu.newtaburl
 						});
-						browser.tabs.remove(newtab.id);
+						browser.tabs.remove(newtab.id).then(() => {
+                            browser.sessions.getRecentlyClosed().then((l) => {
+                                if (Object.prototype.hasOwnProperty.call(l[0],'tab') &&
+                                    l[0].tab.title === '@NewTab') {
+                                    browser.sessions.forgetClosedTab(
+                                        l[0].tab.windowId,
+                                        l[0].tab.sessionId.toString()
+                                    );
+                                }
+                            });
+                        });
 					} else {
 						browser.tabs.update(tabId, {
 							'url': ntu.newtaburl
