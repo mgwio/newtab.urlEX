db30933e0cdf8bf7ac847fc60a1c0e83019fdcb5
Mike Wu <dev@mgw.io>
2018-01-24 13:23:15 -0500

    Fix spacing

--- tabmon.js
+++ tabmon.js
@@ -1,45 +1,43 @@
 
 const newtaburlEX = {
-    async idleCaptive(newtab) {
-        browser.tabs.onUpdated.addListener(newtaburlEX.forward);
-    },
+	async idleCaptive(newtab) {
+    	browser.tabs.onUpdated.addListener(newtaburlEX.forward);
+	},
 
-    async forward(tabId, changeInfo, tab) {
-        console.log("wewled");
-        if (changeInfo.title === '@NewTab') {
-            let ntu = await newtaburlEX.getForwardUrl();
-            let makeactive = await browser.storage.local.get('active');
-            if (makeactive.active) {
-                browser.tabs.create({
-                    'url': ntu
-                });
-                await browser.tabs.remove(tabId)
-                let closed = await browser.sessions.getRecentlyClosed();
-                if (Object.prototype.hasOwnProperty.call(closed[0],'tab') &&
-                    closed[0].tab.title === '@NewTab') {
-                    browser.sessions.forgetClosedTab(
-                        closed[0].tab.windowId,
-                        closed[0].tab.sessionId.toString()
-                    );
-                }
-            } else {
-                await browser.tabs.update(tabId, {
-                    'url': ntu
-                });
-
-            }
-        }
+	async forward(tabId, changeInfo, tab) {
+		if (changeInfo.title === '@NewTab') {
+			let nturl = await newtaburlEX.getForwardUrl();
+			let ntu = await browser.storage.local.get();
+			if (ntu.active) {
+				browser.tabs.create({
+					url: nturl
+				});
+			    await browser.tabs.remove(tabId)
+				let closed = await browser.sessions.getRecentlyClosed();
+				if (Object.prototype.hasOwnProperty.call(closed[0], 'tab') &&
+					closed[0].tab.title === '@NewTab') {
+					browser.sessions.forgetClosedTab(
+						closed[0].tab.windowId,
+						closed[0].tab.sessionId.toString()
+					);
+				}
+			} else {
+				await browser.tabs.update(tabId, {
+					url: nturl
+				});
+			}
+		}
         browser.tabs.onUpdated.removeListener(newtaburlEX.forward);
-    },
+	},
 
-    async getForwardUrl() {
-        let ntu = await browser.storage.local.get('newtaburl');
-        if (ntu.newtaburl == null) {
-            return 'about:blank';
-        } else {
-            return ntu.newtaburl;
-        }
-    }
-}
+	async getForwardUrl() {
+		let newtaburl = await browser.storage.local.get();
+		if (newtaburl.ntu === null) {
+			return 'about:blank';
+		} else {
+			return newtaburl.ntu;
+		}
+	}
+};
 
 browser.tabs.onCreated.addListener(newtaburlEX.idleCaptive);
