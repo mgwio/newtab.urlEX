d6280aed374a2a27db47c9dc48b4626cae0abf31
Mike Wu <dev@mgw.io>
2018-01-24 01:20:52 -0500

    Rewrite listener to use async/await

--- tabmon.js
+++ tabmon.js
@@ -1,42 +1,45 @@
 
-function nt (newtab) {
-	function waitForURL(tabId, changeInfo, tab) {
-		if (changeInfo.title === '@NewTab') {
-			browser.storage.local.get('newtaburl').then((ntu) => {
-				browser.tabs.onUpdated.removeListener(waitForURL);
-				if (ntu.newtaburl == null) {
-					browser.storage.local.set({
-						newtaburl: 'about:blank'
-					});
-					ntu.newtaburl = 'about:blank';
-				}
-				browser.storage.local.get('active').then((act) => {
-					if (act.active) {
-						browser.tabs.create({
-							'url': ntu.newtaburl
-						});
-						browser.tabs.remove(newtab.id).then(() => {
-                            browser.sessions.getRecentlyClosed().then((l) => {
-                                if (Object.prototype.hasOwnProperty.call(l[0],'tab') &&
-                                    l[0].tab.title === '@NewTab') {
-                                    browser.sessions.forgetClosedTab(
-                                        l[0].tab.windowId,
-                                        l[0].tab.sessionId.toString()
-                                    );
-                                }
-                            });
-                        });
-					} else {
-						browser.tabs.update(tabId, {
-							'url': ntu.newtaburl
-						});
-					}
-				});
-			});
-		}
-		browser.tabs.onUpdated.removeListener(waitForURL);
-	}
-	browser.tabs.onUpdated.addListener(waitForURL);
+const newtaburlEX = {
+    async idleCaptive(newtab) {
+        browser.tabs.onUpdated.addListener(newtaburlEX.forward);
+    },
+
+    async forward(tabId, changeInfo, tab) {
+        console.log("wewled");
+        if (changeInfo.title === '@NewTab') {
+            let ntu = await newtaburlEX.getForwardUrl();
+            let makeactive = await browser.storage.local.get('active');
+            if (makeactive.active) {
+                browser.tabs.create({
+                    'url': ntu
+                });
+                await browser.tabs.remove(tabId)
+                let closed = await browser.sessions.getRecentlyClosed();
+                if (Object.prototype.hasOwnProperty.call(closed[0],'tab') &&
+                    closed[0].tab.title === '@NewTab') {
+                    browser.sessions.forgetClosedTab(
+                        closed[0].tab.windowId,
+                        closed[0].tab.sessionId.toString()
+                    );
+                }
+            } else {
+                await browser.tabs.update(tabId, {
+                    'url': ntu
+                });
+
+            }
+        }
+        browser.tabs.onUpdated.removeListener(newtaburlEX.forward);
+    },
+
+    async getForwardUrl() {
+        let ntu = await browser.storage.local.get('newtaburl');
+        if (ntu.newtaburl == null) {
+            return 'about:blank';
+        } else {
+            return ntu.newtaburl;
+        }
+    }
 }
 
-browser.tabs.onCreated.addListener(nt);
+browser.tabs.onCreated.addListener(newtaburlEX.idleCaptive);
